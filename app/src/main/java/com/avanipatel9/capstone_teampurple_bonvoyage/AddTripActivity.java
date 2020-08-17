752c9480165f767801d9d3c1d2e811cc3a4c3359
Jyothithomas <jyothithomas.05@gmail.com>
2020-08-17 19:28:37 -0400

    [JT] add trip activity variables added

--- app/src/main/java/com/avanipatel9/capstone_teampurple_bonvoyage/AddTripActivity.java
+++ app/src/main/java/com/avanipatel9/capstone_teampurple_bonvoyage/AddTripActivity.java
@@ -22,7 +22,17 @@ import java.util.Date;
 import java.util.Random;
 
 public class AddTripActivity extends AppCompatActivity {
+    private DatabaseReference mDatabase,m2Database;
 
+    private String Destination,FriendPhone1,FriendPhone2,FriendPhone3;
+    private int budget;
+    private String DateOfReturn, DateOfDeparture;
+
+    private static final String _CHAR = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+    private static final int RANDOM_STR_LENGTH = 12;
+
+    private EditText t_Destination,t_FriendPhone1,t_FriendPhone2,t_FriendPhone3,t_budget,t_DateOfReturn,t_DateOfDeparture;
+    private Button submit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
