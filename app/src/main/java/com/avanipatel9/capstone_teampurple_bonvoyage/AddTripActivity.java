5816bc9d79dbfd31ea6c87af2e32cfe87ecef661
Jyothithomas <jyothithomas.05@gmail.com>
2020-08-17 19:27:47 -0400

    [JT] add trip activity XML created

--- app/src/main/java/com/avanipatel9/capstone_teampurple_bonvoyage/AddTripActivity.java
+++ app/src/main/java/com/avanipatel9/capstone_teampurple_bonvoyage/AddTripActivity.java
@@ -1,8 +1,25 @@
 package com.avanipatel9.capstone_teampurple_bonvoyage;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDialogFragment;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.*;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.DatabaseReference;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Random;
 
 public class AddTripActivity extends AppCompatActivity {
 
