93e65e29d9b2af0b0a5d1bbc5e3fcd3d0bb9c766
Charmi <c0768448@mylambton.ca>
2020-08-18 14:56:15 -0400

    [cp] add event listeners to calculate money spent in trip activity

--- app/src/main/java/com/avanipatel9/capstone_teampurple_bonvoyage/TripActivity.java
+++ app/src/main/java/com/avanipatel9/capstone_teampurple_bonvoyage/TripActivity.java
@@ -2,13 +2,20 @@ package com.avanipatel9.capstone_teampurple_bonvoyage;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
@@ -30,5 +37,128 @@ public class TripActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trip);
+
+        ListView myListView = (ListView) findViewById(R.id.mylist);
+        listItems=new ArrayList<String>();
+        adapter=new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1,
+                listItems);
+        myListView.setAdapter(adapter);
+
+
+        mDatabase = FirebaseDatabase.getInstance().getReference("trips");
+        m2Database =  FirebaseDatabase.getInstance().getReference("users");
+
+        GlobalVariables a = (GlobalVariables)getApplication();
+        tripid = a.getTripid();
+        SharedPreferences prefs = getSharedPreferences("phoneandpass", MODE_PRIVATE);
+        phonenumber = prefs.getString("phonenumber", null);
+        Destination = (TextView) findViewById(R.id.destination);
+        Budget = (TextView) findViewById(R.id.moneyspent);
+        submit = (Button) findViewById(R.id.submit);
+        enteredmoney = (EditText) findViewById(R.id.entermoney);
+        moneyspentbyme = (TextView) findViewById(R.id.moneyspentbyme);
+
+//        adapter=new ArrayAdapter<String>(this,
+//                android.R.layout.simple_list_item_1,
+//                listItems);
+
+        mDatabase.child(tripid).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String destination = dataSnapshot.child("Destination").getValue().toString();
+                moneyspent = dataSnapshot.child("moneyspent").getValue().toString();
+                moneybythis = dataSnapshot.child(phonenumber).getValue().toString();
+                Destination.setText(destination);
+                System.out.print(moneyspent + "  " + moneyspentbyme);
+                Budget.setText("total money spent in the group:: " +moneyspent);
+                moneyspentbyme.setText("total money contributed by me::  " + moneybythis);
+                System.out.println("   ");
+                for(DataSnapshot ds : dataSnapshot.getChildren()) {
+                    String name = ds.getKey().toString();
+                    final String m = ds.getValue().toString();
+                    if(name.matches("[1-9][0-9]{9,10}")){
+//                        System.out.println(name);
+                        m2Database.child(name).addListenerForSingleValueEvent(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(DataSnapshot dataSnapshot) {
+                                System.out.println(dataSnapshot.child("Name").getValue()+"  " + m);
+//                                adapter.add(m);
+                                String name = (String)dataSnapshot.child("Name").getValue();
+                                listItems.add(name + "    "  + m);
+                                adapter.notifyDataSetChanged();
+                            }
+                            @Override
+                            public void onCancelled(DatabaseError databaseError) {
+
+                            }
+                        });
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                mDatabase.child(tripid).addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        String destination = dataSnapshot.child("Destination").getValue().toString();
+                        moneyspent = dataSnapshot.child("moneyspent").getValue().toString();
+                        moneybythis = dataSnapshot.child(phonenumber).getValue().toString();
+                        Destination.setText(destination);
+                        Budget.setText("total money spent in the group:: " +moneyspent);
+                        moneyspentbyme.setText("total money contributed by me::  " + moneybythis);
+//                        listItems.remove(0);
+                        listItems.clear();
+                        for(DataSnapshot ds : dataSnapshot.getChildren()) {
+                            String name = ds.getKey().toString();
+                            final String m = ds.getValue().toString();
+                            if(name.matches("[1-9][0-9]{9,10}")){
+//                        System.out.println(name);
+                                m2Database.child(name).addListenerForSingleValueEvent(new ValueEventListener() {
+                                    @Override
+                                    public void onDataChange(DataSnapshot dataSnapshot) {
+                                        System.out.println(dataSnapshot.child("Name").getValue()+"  " + m);
+//                                adapter.add(m);
+                                        String name = (String)dataSnapshot.child("Name").getValue();
+
+                                        listItems.add(name + "    "  + m);
+                                        adapter.notifyDataSetChanged();
+                                    }
+                                    @Override
+                                    public void onCancelled(DatabaseError databaseError) {
+
+                                    }
+                                });
+                            }
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+
+
+                int entered = Integer.parseInt(enteredmoney.getText().toString());
+                int totalforthis = entered + Integer.parseInt(moneybythis);
+                int total = entered + Integer.parseInt(moneyspent);
+                String text = String.valueOf(total);
+                String text1 = String.valueOf(totalforthis);
+                mDatabase.child(tripid).child(phonenumber).setValue(totalforthis);
+                mDatabase.child(tripid).child("moneyspent").setValue(total);
+                Budget.setText("total money spent in the group:: " + text);
+                moneyspentbyme.setText("total money contributed by me::  " + text1);
+            }
+        });
     }
 }
