81d17e17594542001923c030394f6c7240c28db3
Charmi <c0768448@mylambton.ca>
2020-08-18 13:22:25 -0400

    [cp] add trip activity to show money spent and money owed

--- app/src/main/java/com/avanipatel9/capstone_teampurple_bonvoyage/AddTripActivity.java
+++ app/src/main/java/com/avanipatel9/capstone_teampurple_bonvoyage/AddTripActivity.java
@@ -8,7 +8,6 @@ import android.app.Dialog;
 import android.app.DialogFragment;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.*;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
