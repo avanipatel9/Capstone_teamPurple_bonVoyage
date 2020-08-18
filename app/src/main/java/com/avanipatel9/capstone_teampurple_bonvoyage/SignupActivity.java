b0dcfffd4449bf4c88534958f2ead9d7957c3fe5
Avani Patel <anpatel36@yahoo.in>
2020-08-17 20:04:11 -0400

    [AP] variables added in SignupActivity

--- app/src/main/java/com/avanipatel9/capstone_teampurple_bonvoyage/SignupActivity.java
+++ app/src/main/java/com/avanipatel9/capstone_teampurple_bonvoyage/SignupActivity.java
@@ -3,8 +3,20 @@ package com.avanipatel9.capstone_teampurple_bonvoyage;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class SignupActivity extends AppCompatActivity {
+
+    private DatabaseReference mDatabase,databaseref;
+    private Button submitButton;
+    private EditText et_name,et_email,et_password,et_confirmPassword,et_city;
+    private String Name,Email,Password,ConfirmPassword,City;
+    private TextView message;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
