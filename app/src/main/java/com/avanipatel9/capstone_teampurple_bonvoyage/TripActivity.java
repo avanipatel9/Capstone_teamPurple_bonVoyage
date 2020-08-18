b241c6a13918de6654261c6b749fd47fd24d5a66
Charmi <c0768448@mylambton.ca>
2020-08-18 13:25:38 -0400

    [cp] initalize variables in tripActivity class

--- app/src/main/java/com/avanipatel9/capstone_teampurple_bonvoyage/TripActivity.java
+++ app/src/main/java/com/avanipatel9/capstone_teampurple_bonvoyage/TripActivity.java
@@ -3,8 +3,28 @@ package com.avanipatel9.capstone_teampurple_bonvoyage;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.database.DatabaseReference;
+
+import java.util.ArrayList;
 
 public class TripActivity extends AppCompatActivity {
+    private DatabaseReference mDatabase,m2Database;
+    private String tripid, budget2;
+    private String phonenumber;
+    private String moneyspent;
+    private TextView Budget;
+    private TextView Destination,moneyspentbyme;
+    private Button submit;
+    private EditText enteredmoney;
+    private String moneybythis;
+
+    private ArrayAdapter adapter;
+    ArrayList<String> listItems;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
