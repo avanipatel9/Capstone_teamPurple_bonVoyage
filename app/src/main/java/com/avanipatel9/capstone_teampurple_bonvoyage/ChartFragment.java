1ff53a9b1c59cf01bd03d3e7e471d92edaa2e015
anmariyafrancis <anmariya1915@gmail.com>
2020-08-22 15:06:13 -0400

    [AF]declared textview variables in ChartFragment.java

--- app/src/main/java/com/avanipatel9/capstone_teampurple_bonvoyage/ChartFragment.java
+++ app/src/main/java/com/avanipatel9/capstone_teampurple_bonvoyage/ChartFragment.java
@@ -1,15 +1,16 @@
 package com.avanipatel9.capstone_teampurple_bonvoyage;
 
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
 public class ChartFragment extends Fragment {
-private View chartView;
-//private PieChart pieChart1, pieChart2, pieChart3;
+    private View chartView;
+    //private PieChart pieChart1, pieChart2, pieChart3;
+    private TextView pieChart1Title, pieChart2Title, pieChart3Title;
 
-public ChartFragment()
-{
-}
-
+    public ChartFragment()
+    {
+    }
 }
