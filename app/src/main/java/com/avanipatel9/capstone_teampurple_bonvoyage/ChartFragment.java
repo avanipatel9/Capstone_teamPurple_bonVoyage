6512559e0c0fe5921979c494565b92ca7eae17ff
anmariyafrancis <anmariya1915@gmail.com>
2020-08-22 15:04:53 -0400

    [AF]created empty constructor class in ChartFragment.java

--- app/src/main/java/com/avanipatel9/capstone_teampurple_bonvoyage/ChartFragment.java
+++ app/src/main/java/com/avanipatel9/capstone_teampurple_bonvoyage/ChartFragment.java
@@ -1,6 +1,15 @@
 package com.avanipatel9.capstone_teampurple_bonvoyage;
 
+import android.view.View;
+
 import androidx.fragment.app.Fragment;
 
 public class ChartFragment extends Fragment {
+private View chartView;
+//private PieChart pieChart1, pieChart2, pieChart3;
+
+public ChartFragment()
+{
+}
+
 }
