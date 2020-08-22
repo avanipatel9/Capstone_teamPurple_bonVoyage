fcbb9af5df52f5e3bbb5785f85c2c0cbb39507d8
anmariyafrancis <anmariya1915@gmail.com>
2020-08-22 15:30:23 -0400

    [AF]created oncreateview in ChartFragment.java

--- app/src/main/java/com/avanipatel9/capstone_teampurple_bonvoyage/ChartFragment.java
+++ app/src/main/java/com/avanipatel9/capstone_teampurple_bonvoyage/ChartFragment.java
@@ -1,8 +1,13 @@
 package com.avanipatel9.capstone_teampurple_bonvoyage;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class ChartFragment extends Fragment {
@@ -16,5 +21,18 @@ public class ChartFragment extends Fragment {
     public static ChartFragment newInstance(){
         ChartFragment fragment= new ChartFragment();
         return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater,ViewGroup container,Bundle savedInstanceState) {
+        chartView = inflater.inflate(R.layout.fragment_chart,container,false);
+
+        findViewsById();
+        loadSummary();
+        return chartView;
+    }
+    private void loadSummary() {
+    }
+    private void findViewsById() {
     }
 }
