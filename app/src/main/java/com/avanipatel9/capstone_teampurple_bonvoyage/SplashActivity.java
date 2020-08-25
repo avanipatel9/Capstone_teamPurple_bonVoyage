b76ea4e81e82b1174cde801eddef08d974c00f54
anmariyafrancis <anmariya1915@gmail.com>
2020-08-24 20:26:50 -0400

    [AF]variable created for time value in SplashActivity.java

--- app/src/main/java/com/avanipatel9/capstone_teampurple_bonvoyage/SplashActivity.java
+++ app/src/main/java/com/avanipatel9/capstone_teampurple_bonvoyage/SplashActivity.java
@@ -3,6 +3,7 @@ package com.avanipatel9.capstone_teampurple_bonvoyage;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        int secondsDelayed=1;
+        new Handler().postDelayed()
     }
 }
