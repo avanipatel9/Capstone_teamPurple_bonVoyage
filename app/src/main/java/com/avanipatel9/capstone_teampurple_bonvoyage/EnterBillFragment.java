e1c61ca202c4bbed5a53d2e2f494074edc177560
anmariyafrancis <anmariya1915@gmail.com>
2020-08-27 19:11:14 -0400

    [AF]declared listview variable

--- app/src/main/java/com/avanipatel9/capstone_teampurple_bonvoyage/EnterBillFragment.java
+++ app/src/main/java/com/avanipatel9/capstone_teampurple_bonvoyage/EnterBillFragment.java
@@ -2,11 +2,18 @@ package com.avanipatel9.capstone_teampurple_bonvoyage;
 
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.List;
 
 import androidx.fragment.app.Fragment;
 
 public class EnterBillFragment extends Fragment implements View.OnClickListener, AdapterView.OnItemClickListener {
     private EditText billTitle;
     private EditText billAmount;
+    private ListView friendsListView;
+    private Button addBillButton;
+
 }
