12fa2601a0011962b1a9cc8cc935b3d63aa887e1
anmariyafrancis <anmariya1915@gmail.com>
2020-08-27 19:07:23 -0400

    [AF]declared variables EnterBillFragment

--- app/src/main/java/com/avanipatel9/capstone_teampurple_bonvoyage/EnterBillFragment.java
+++ app/src/main/java/com/avanipatel9/capstone_teampurple_bonvoyage/EnterBillFragment.java
@@ -1,6 +1,12 @@
 package com.avanipatel9.capstone_teampurple_bonvoyage;
 
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.EditText;
+
 import androidx.fragment.app.Fragment;
 
-public class EnterBillFragment extends Fragment {
+public class EnterBillFragment extends Fragment implements View.OnClickListener, AdapterView.OnItemClickListener {
+    private EditText billTitle;
+    private EditText billAmount;
 }
