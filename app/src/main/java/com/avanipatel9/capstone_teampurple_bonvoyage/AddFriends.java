0f64df8ec7681db9db129260f74ef024fd307294
anmariyafrancis <anmariya1915@gmail.com>
2020-08-27 20:11:36 -0400

    [AF]created constructor class

--- app/src/main/java/com/avanipatel9/capstone_teampurple_bonvoyage/AddFriends.java
+++ app/src/main/java/com/avanipatel9/capstone_teampurple_bonvoyage/AddFriends.java
@@ -2,10 +2,16 @@ package com.avanipatel9.capstone_teampurple_bonvoyage;
 
 import android.widget.AdapterView;
 import android.widget.EditText;
+import android.widget.ListView;
 
 import androidx.fragment.app.Fragment;
 
 public class AddFriends extends Fragment implements AdapterView.OnItemClickListener {
     private EditText searchQuery;
+    private ListView usersListView;
+    private View view;
+    private UsersListViewAdapter adapter;
 
+    public AddFriends(){
+    }
 }
