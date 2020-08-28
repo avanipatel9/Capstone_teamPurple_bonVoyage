f8b5731dc75560c3b349349be7cd96bdac1e9f25
anmariyafrancis <anmariya1915@gmail.com>
2020-08-27 21:13:06 -0400

    [AF]created new function setUpListView

--- app/src/main/java/com/avanipatel9/capstone_teampurple_bonvoyage/AddFriends.java
+++ app/src/main/java/com/avanipatel9/capstone_teampurple_bonvoyage/AddFriends.java
@@ -8,6 +8,8 @@ import android.widget.AdapterView;
 import android.widget.EditText;
 import android.widget.ListView;
 
+import java.util.ArrayList;
+
 import androidx.fragment.app.Fragment;
 
 public class AddFriends extends Fragment implements AdapterView.OnItemClickListener {
@@ -24,7 +26,14 @@ public class AddFriends extends Fragment implements AdapterView.OnItemClickListe
     }
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
-        view =inflater.inflate(R.layout.)
+        //view =inflater.inflate();
+        setUpListView();
+        return view;
+    }
+    private void setUpListView(){
+        usersDataModels=new ArrayList<>();
+        filteredUsersDataModel=new ArrayList<>();
+
     }
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
